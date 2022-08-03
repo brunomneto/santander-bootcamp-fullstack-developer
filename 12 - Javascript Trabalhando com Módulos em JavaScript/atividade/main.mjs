ab2217f4bd2423dc3a9158399d3932550c5d5ae0
brunomneto <mullernetobruno@gmail.com>
2022-08-02 21:31:54 -0300

    Novas aulas

---
+++ 12 - Javascript Trabalhando com Módulos em JavaScript/atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Bruno", 29));
+console.log(mostraCidade("Bruno", "Porto Alegre"));
+console.log(mostraHobby("Bruno", "Ver séries"));
